2b7ccf17cb34c637a9166ec2963300544c9f50b3
Zoltan151 <32231430+zoltan151@users.noreply.github.com>
2024-04-17 11:11:00 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Install Rustc version 1.59 ##
+curl https://sh.rustup.rs -sSf | sh; source $HOME/.cargo/env | rustup install 1.59 | rustup default 1.59
+
+
+## Install cmake 3.2.2 ##
+sudo apt-get install build-essential
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+tar xf cmake-3.2.2.tar.gz
+cd cmake-3.2.2
+./configure
+make
+sudo make installmake-3.2.2.tar.gz cd cmake-3.2.2 ./configure make sudo make install
+
+
+## Clone GIT and compile ##
+git clone https://github.com/zoltan151/MWC-miner.git
+cd MWC-miner
+git submodule update --init
+cargo build
+
